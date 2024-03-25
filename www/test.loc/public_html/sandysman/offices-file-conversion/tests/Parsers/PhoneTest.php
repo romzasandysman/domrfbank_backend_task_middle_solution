252107ccc90af8cbf607180be9f45cdb35f2d4d8
Alexander <romzasandysman@gmail.com>
2024-03-25 18:08:05 +0700

    Написал тест для конвертации телефона в формат телефона страны

--- www/test.loc/public_html/sandysman/offices-file-conversion/tests/Parsers/PhoneTest.php
+++ www/test.loc/public_html/sandysman/offices-file-conversion/tests/Parsers/PhoneTest.php
@@ -8,16 +8,55 @@ use PHPUnit\Framework\TestCase;
 final class PhoneTest extends TestCase
 {
     #[DataProvider('providerOfPhones')]
-    public function testGetCountryNumber(string $phone)
+    public function testGetCountryNumber(array $testPhones)
     {
-        $this->assertEquals("8-800-775-86-90", $phone);
+        $result = "";
+
+        if ($testPhones["PHONE"]) {
+            foreach ($this->getRulesForPhones() as $rule) {
+                if (preg_match($rule["PATTERN"], $testPhones["PHONE"])) {
+                    $result = $rule["GET_COUNTRY_NUMBER"]($testPhones["PHONE"]);
+                }
+            }
+        }
+
+        $this->assertEquals($testPhones["EXPECT"], $result);
+    }
+
+    /**
+     * Храним правила для телефонов, для проверки
+     * @return array[]
+     */
+    private function getRulesForPhones(): array
+    {
+        return [
+            [
+                "PATTERN"            => "/\d{1}-\d{3}-\d{3}-\d{2}-\d{2}$/",
+                "GET_COUNTRY_NUMBER" => fn(string $phone) => substr(str_replace("-", "", $phone), 1)
+            ]
+        ];
     }
 
     public static function providerOfPhones(): array
     {
         return [
             [
-                "8-800-775-86-90"
+                [
+                    "PHONE"     => "8-800-775-86-90",
+                    "EXPECT"    => "8007758690"
+                ],
+            ],
+            [
+                [
+                    "PHONE"     => "8-800-775-86-89",
+                    "EXPECT"    => "8007758689"
+                ],
+            ],
+            [
+                [
+                    "PHONE"     => "8-800-775-86-86",
+                    "EXPECT"    => "8007758686"
+                ]
             ]
         ];
     }
